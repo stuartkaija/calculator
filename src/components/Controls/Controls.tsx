901a6af8e4b4a82ed063d1ed0631eed39880dbbd
Stuart Kaija <stuartkaija@gmail.com>
2022-05-04 16:01:22 -0700

    simplified buttons (removed button components), basic CSS grid set up for controls

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -4,12 +4,27 @@ import './Controls.scss';
 import NumberButton from '../NumberButton/NumberButton.tsx';
 
 export default function Controls() {
+
     return (
         <div className='controls'>
-            <NumberButton />
-            <NumberButton />
-            <NumberButton />
-
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>DEL</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button>/</button>
+            <button>x</button>
+            <button>RESET</button>
+            <button>=</button>
         </div>
     )
 }
