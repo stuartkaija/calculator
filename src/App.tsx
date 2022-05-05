008ae7b76cd3b9d2a85baa6f22f2b17226f6ec23
Stuart Kaija <stuartkaija@gmail.com>
2022-05-05 09:15:48 -0700

    numbers appearing on display, reset and delete functioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
 
 //@ts-ignore
@@ -7,16 +7,68 @@ import Header from './components/Header/Header.tsx';
 import Display from './components/Display/Display.tsx';
 //@ts-ignore
 import Controls from './components/Controls/Controls.tsx';
+import { stringify } from 'querystring';
 
 function App() {
 
-	const [display, setDisplay] = useState(0);
+	const [display, setDisplay] = useState('0');
+
+	const [equation, setEqution] = useState(null)
+	
+	// more state to hold display number once user clicks operator button
+	// such that the second number they start clicking in appears in display
+
+	// display handler
+	const handleNumber = (number) => {
+
+		// check if display contains a decimel
+		if (number === '.' && display.includes('.')) {
+			return;
+		}
+		
+		if (display === '0') {
+			setDisplay(number);
+		} else {
+			setDisplay(prevDisplay => prevDisplay + number)
+		}
+	};
+
+	// reset handler
+	const handleReset = () => {
+		setDisplay('0');
+	};
+
+	// mathematical function handler
+	const handleOperator = (operator) => {
+		// convert string num to number
+		// operator will be either + - x /
+		console.log(operator)
+
+		// setEquation equal to display
+
+	};
+
+	const handleDelete = () => {
+		if (display.length === 1) {
+			setDisplay('0')
+			return
+		}
+		setDisplay(prevDisplay => prevDisplay.slice(0, -1));
+	}
+
+
+	// handleEquation function for equals button? 
 
 	return (
 		<div className="App">
 			<Header />
 			<Display display={display} />
-			<Controls />
+			<Controls 
+				handleNumber={handleNumber}
+				handleReset={handleReset}
+				handleOperator={handleOperator}
+				handleDelete={handleDelete}
+			/>
 		</div>
 	);
 }
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import './Controls.scss';
-//@ts-ignore
-import NumberButton from '../NumberButton/NumberButton.tsx';
 
-export default function Controls() {
+export default function Controls({handleNumber, handleReset, handleOperator, handleDelete}) {
 
     return (
         <div className='controls'>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>DEL</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
-            <button>/</button>
-            <button>x</button>
-            <button>RESET</button>
+            <button onClick={()=>{handleNumber('7')}}>7</button>
+            <button onClick={()=>{handleNumber('8')}}>8</button>
+            <button onClick={()=>{handleNumber('9')}}>9</button>
+            <button onClick={()=>{handleDelete()}}>DEL</button>
+            <button onClick={()=>{handleNumber('4')}}>4</button>
+            <button onClick={()=>{handleNumber('5')}}>5</button>
+            <button onClick={()=>{handleNumber('6')}}>6</button>
+            <button onClick={()=>{handleOperator('+')}}>+</button>
+            <button onClick={()=>{handleNumber('1')}}>1</button>
+            <button onClick={()=>{handleNumber('2')}}>2</button>
+            <button onClick={()=>{handleNumber('3')}}>3</button>
+            <button onClick={()=>{handleOperator('-')}}>-</button>
+            <button onClick={()=>{handleNumber('.')}}>.</button>
+            <button onClick={()=>{handleNumber('0')}}>0</button>
+            <button onClick={()=>{handleOperator('/')}}>/</button>
+            <button onClick={()=>{handleOperator('x')}}>x</button>
+            <button onClick={()=>{handleReset()}}>RESET</button>
             <button>=</button>
         </div>
     )
