e9a85eb75d4829d7aeb5fb4388ecf12f3b3a41a6
Stuart Kaija <stuartkaija@gmail.com>
2022-05-05 09:35:44 -0700

    equals handler functioning on basic level with addition

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,18 @@ function App() {
 
 	const [display, setDisplay] = useState('0');
 
-	const [equation, setEqution] = useState(null)
+	const [equation, setEquation] = useState(null)
 	
 	// more state to hold display number once user clicks operator button
 	// such that the second number they start clicking in appears in display
 
 	// display handler
 	const handleNumber = (number) => {
+
+		// check if equation is truthy, if yes, that means user has clicked an operator so display must reset to 0
+		// if (equation) {
+		// 	setDisplay('0')
+		// }
 
 		// check if display contains a decimel
 		if (number === '.' && display.includes('.')) {
@@ -36,16 +41,23 @@ function App() {
 	// reset handler
 	const handleReset = () => {
 		setDisplay('0');
+		setEquation(null)
 	};
 
 	// mathematical function handler
 	const handleOperator = (operator) => {
-		// convert string num to number
-		// operator will be either + - x /
-		console.log(operator)
 
+		// check if display and equation have numbers, i.e. user is doing multiple operations
+
+
+		// check if display === 0, if so ignore
+		if (display === '0') {
+			return
+		}
 		// setEquation equal to display
-
+		setEquation(display);
+		console.log(equation);
+		setDisplay('0')
 	};
 
 	const handleDelete = () => {
@@ -56,8 +68,17 @@ function App() {
 		setDisplay(prevDisplay => prevDisplay.slice(0, -1));
 	}
 
-
 	// handleEquation function for equals button? 
+	const handleEquals = () => {
+		if (equation) {
+			// convert display and equation to numbers
+			const equals = parseInt(equation) + parseInt(display)
+
+			setDisplay(equals.toString())
+
+			// check what operator was (may need more state for this?)
+		}
+	}
 
 	return (
 		<div className="App">
@@ -65,9 +86,10 @@ function App() {
 			<Display display={display} />
 			<Controls 
 				handleNumber={handleNumber}
-				handleReset={handleReset}
 				handleOperator={handleOperator}
+				handleEquals={handleEquals}
 				handleDelete={handleDelete}
+				handleReset={handleReset}
 			/>
 		</div>
 	);
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import './Controls.scss';
 
-export default function Controls({handleNumber, handleReset, handleOperator, handleDelete}) {
+export default function Controls({handleNumber, handleReset, handleOperator, handleDelete, handleEquals}) {
 
     return (
         <div className='controls'>
-            <button onClick={()=>{handleNumber('7')}}>7</button>
-            <button onClick={()=>{handleNumber('8')}}>8</button>
-            <button onClick={()=>{handleNumber('9')}}>9</button>
-            <button onClick={()=>{handleDelete()}}>DEL</button>
-            <button onClick={()=>{handleNumber('4')}}>4</button>
-            <button onClick={()=>{handleNumber('5')}}>5</button>
-            <button onClick={()=>{handleNumber('6')}}>6</button>
-            <button onClick={()=>{handleOperator('+')}}>+</button>
-            <button onClick={()=>{handleNumber('1')}}>1</button>
-            <button onClick={()=>{handleNumber('2')}}>2</button>
-            <button onClick={()=>{handleNumber('3')}}>3</button>
-            <button onClick={()=>{handleOperator('-')}}>-</button>
-            <button onClick={()=>{handleNumber('.')}}>.</button>
-            <button onClick={()=>{handleNumber('0')}}>0</button>
-            <button onClick={()=>{handleOperator('/')}}>/</button>
-            <button onClick={()=>{handleOperator('x')}}>x</button>
-            <button onClick={()=>{handleReset()}}>RESET</button>
-            <button>=</button>
+            <button onClick={() => {handleNumber('7')}}>7</button>
+            <button onClick={() => {handleNumber('8')}}>8</button>
+            <button onClick={() => {handleNumber('9')}}>9</button>
+            <button onClick={() => {handleDelete()}}>DEL</button>
+            <button onClick={() => {handleNumber('4')}}>4</button>
+            <button onClick={() => {handleNumber('5')}}>5</button>
+            <button onClick={() => {handleNumber('6')}}>6</button>
+            <button onClick={() => {handleOperator('+')}}>+</button>
+            <button onClick={() => {handleNumber('1')}}>1</button>
+            <button onClick={() => {handleNumber('2')}}>2</button>
+            <button onClick={() => {handleNumber('3')}}>3</button>
+            <button onClick={() => {handleOperator('-')}}>-</button>
+            <button onClick={() => {handleNumber('.')}}>.</button>
+            <button onClick={() => {handleNumber('0')}}>0</button>
+            <button onClick={() => {handleOperator('/')}}>/</button>
+            <button onClick={() => {handleOperator('x')}}>x</button>
+            <button onClick={() => {handleReset()}}>RESET</button>
+            <button onClick={() => {handleEquals()}}>=</button>
         </div>
     )
 }
