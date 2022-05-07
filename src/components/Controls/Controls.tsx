1b9c3aad83bae380dbc02af5862763d10461fdd5
Stuart Kaija <stuartkaija@gmail.com>
2022-05-07 16:50:28 -0700

    styling buttons underway, continuing with mobile styling

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -5,24 +5,24 @@ export default function Controls({handleNumber, handleReset, handleOperator, han
 
     return (
         <div className='controls'>
-            <button onClick={() => {handleNumber('7')}}>7</button>
-            <button onClick={() => {handleNumber('8')}}>8</button>
-            <button onClick={() => {handleNumber('9')}}>9</button>
-            <button onClick={() => {handleDelete()}}>DEL</button>
-            <button onClick={() => {handleNumber('4')}}>4</button>
-            <button onClick={() => {handleNumber('5')}}>5</button>
-            <button onClick={() => {handleNumber('6')}}>6</button>
-            <button onClick={() => {handleOperator('+')}}>+</button>
-            <button onClick={() => {handleNumber('1')}}>1</button>
-            <button onClick={() => {handleNumber('2')}}>2</button>
-            <button onClick={() => {handleNumber('3')}}>3</button>
-            <button onClick={() => {handleOperator('-')}}>-</button>
-            <button onClick={() => {handleNumber('.')}}>.</button>
-            <button onClick={() => {handleNumber('0')}}>0</button>
-            <button onClick={() => {handleOperator('/')}}>/</button>
-            <button onClick={() => {handleOperator('x')}}>x</button>
-            <button onClick={() => {handleReset()}}>RESET</button>
-            <button onClick={() => {handleEquals()}}>=</button>
+            <button className='controls__button' onClick={() => {handleNumber('7')}}>7</button>
+            <button className='controls__button' onClick={() => {handleNumber('8')}}>8</button>
+            <button className='controls__button' onClick={() => {handleNumber('9')}}>9</button>
+            <button className='controls__button' onClick={() => {handleDelete()}}>DEL</button>
+            <button className='controls__button' onClick={() => {handleNumber('4')}}>4</button>
+            <button className='controls__button' onClick={() => {handleNumber('5')}}>5</button>
+            <button className='controls__button' onClick={() => {handleNumber('6')}}>6</button>
+            <button className='controls__button' onClick={() => {handleOperator('+')}}>+</button>
+            <button className='controls__button' onClick={() => {handleNumber('1')}}>1</button>
+            <button className='controls__button' onClick={() => {handleNumber('2')}}>2</button>
+            <button className='controls__button' onClick={() => {handleNumber('3')}}>3</button>
+            <button className='controls__button' onClick={() => {handleOperator('-')}}>-</button>
+            <button className='controls__button' onClick={() => {handleNumber('.')}}>.</button>
+            <button className='controls__button' onClick={() => {handleNumber('0')}}>0</button>
+            <button className='controls__button' onClick={() => {handleOperator('/')}}>/</button>
+            <button className='controls__button' onClick={() => {handleOperator('x')}}>x</button>
+            <button className='controls__button' onClick={() => {handleReset()}}>RESET</button>
+            <button className='controls__button' onClick={() => {handleEquals()}}>=</button>
         </div>
     )
 }
